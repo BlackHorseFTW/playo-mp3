117481295b75cc7f095940bc00dd6f3ae40e04d9
ashvith <ashvithadepu89@gmail.com>
2025-04-02 15:50:05 +0530

    commit

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "../trpc";
 import { posts } from "../../db/schema";
 import { desc } from "drizzle-orm";
 
